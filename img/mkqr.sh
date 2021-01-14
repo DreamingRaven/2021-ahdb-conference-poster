764ad0bd80fed1806f815473d47a266e5a072f04
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-01-14 12:17:29 +0000

    Updated summary and qr codes

--- img/mkqr.sh
+++ img/mkqr.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 # @Author: GeorgeRaven <archer>
 # @Date:   2020-12-01T12:12:37+00:00
 # @Last modified by:   archer
-# @Last modified time: 2020-12-17T19:30:10+00:00
+# @Last modified time: 2021-01-14T12:11:43+00:00
 # @License: please see LICENSE file in project root
 
 
 
 # sudo pacman -S qrencode
-qrencode -o qrcode.png -s 4 https://github.com/DreamingRaven/encrypted-deep-learning-presentation
+qrencode -o qrcode.png -s 4 https://github.com/DreamingRaven/2021-ahdb-conference-poster
